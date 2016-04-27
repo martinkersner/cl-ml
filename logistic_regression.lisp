1817c362928ace1f4a386b729725ef857b818172
Martin Keršner <m.kersner@gmail.com>
2016-04-27 18:15:22 +0900

    stochastic gradient descent in progress

--- logistic_regression.lisp
+++ logistic_regression.lisp
@@ -30,3 +30,43 @@
       (setf weights (add weights (dot (multiply lr data_matrix_T) err))))
 
   weights))
+
+(defun stochastic-grad-ascent (data_matrix labels_matrix &key (max_iter 500))
+  (let* ((data_matrix (append-const-val data_matrix 1.0))
+         (m (array-dimension data_matrix 0))
+         (n (array-dimension data_matrix 1))
+         (weights (make-array (list n 1) :initial-element 1))
+         (h)
+         (err)
+         (output))
+
+    (dotimes (i max_iter)
+      (let ((data_index (iota m))
+            (rand_index)
+            (lr)
+            (current_data)
+            (current_label))
+
+      (dotimes(j m)
+        (setf lr (+ 0.01 (/ 4 (+ 1.0 i j)))) ; why 4?
+        (setf rand_index (random (length data_index)))
+        (setf current_data (array-row-slice data_matrix rand_index))
+        (setf current_label (make-array '(1 1) :initial-element (aref labels_matrix rand_index 0)))
+
+        ;(setf h (sigmoid (dot current_data weights)))
+        ;(setf err (subtract current_label h))
+        ;(setf weights (add weights (dot (multiply lr current_data) err)))
+
+        (setf data_index (remove-nth rand_index data_index))
+        )
+
+  ;(setf output
+  ;weights
+  ;current_data
+  ;)
+     )
+      )
+
+  ;output
+  weights
+   ))
