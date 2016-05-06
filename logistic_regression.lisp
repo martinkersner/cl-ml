c208273cf035656c09431f865a1f4ec42808c5c2
Martin <m.kersner@gmail.com>
2016-05-06 14:47:21 +0900

    modified stochastic gradient ascent in order to employ math2 lib

--- logistic_regression.lisp
+++ logistic_regression.lisp
@@ -13,14 +13,12 @@
 (defparameter *labels* (matrix-from-data (nth-col *label-col* *dataset2*)))
 
 (defun grad-ascent (data_mat labels_mat &key (lr 0.001) (max_iter 500))
-  (let* (
-         (data_mat (prefix-const-val 1.0 data_mat))
+  (let* ((data_mat (prefix-const-val 1.0 data_mat))
          (n (matrix-cols data_mat))
          (weights (initialize-matrix n 1 1))
          (data_mat_T (transpose data_mat))
          (h)
-         (err)
-         )
+         (err))
 
     (dotimes (i max_iter)
       (setf h (sigmoid (dot data_mat weights)))
@@ -29,42 +27,42 @@
 
   weights))
 
-;(defun stochastic-grad-ascent (data_matrix labels_matrix &key (max_iter 500))
-;  (let* ((data_matrix (append-const-val data_matrix 1.0))
-;         (m (array-dimension data_matrix 0))
-;         (n (array-dimension data_matrix 1))
-;         (weights (make-array (list n 1) :initial-element 1))
-;         (h)
-;         (err)
-;         (output))
-;
-;    (dotimes (i max_iter)
-;      (let ((data_index (iota m))
-;            (rand_index)
-;            (lr)
-;            (current_data)
-;            (current_label))
-;
-;      (dotimes(j m)
-;        (setf lr (+ 0.01 (/ 4 (+ 1.0 i j)))) ; why 4?
-;        (setf rand_index (random (length data_index)))
-;        (setf current_data (array-row-slice data_matrix rand_index))
-;        (setf current_label (make-array '(1 1) :initial-element (aref labels_matrix rand_index 0)))
-;
-;        ;(setf h (sigmoid (dot current_data weights)))
-;        ;(setf err (subtract current_label h))
-;        ;(setf weights (add weights (dot (multiply lr current_data) err)))
-;
-;        (setf data_index (remove-nth rand_index data_index))
-;        )
-;
-;  ;(setf output
-;  ;weights
-;  ;current_data
-;  ;)
-;     )
-;      )
-;
-;  ;output
-;  weights
-;   ))
+(defun stochastic-grad-ascent (data_matrix labels_matrix &key (max_iter 500))
+  (let* ((data_mat (append-const-val 1.0 data_matrix))
+         (m (matrix-rows data_mat))
+         (n (matrix-cols data_mat))
+         (weights (initialize-matrix n 1 1))
+         (h)
+         (err)
+         (output))
+
+    (dotimes (i max_iter)
+      (let ((data_index (iota m))
+            (rand_index)
+            (lr)
+            (current_data)
+            (current_label))
+
+      (dotimes(j m)
+        (setf lr (+ 0.01 (/ 4 (+ 1.0 i j)))) ; why 4?
+        (setf rand_index (random (length data_index)))
+        (setf current_data (nth-row rand_index data_mat))
+        (setf current_label (nth-row rand_index label_mat))
+
+        ;(setf h (sigmoid (dot current_data weights)))
+        ;(setf err (subtract current_label h))
+        ;(setf weights (add weights (dot (multiply lr current_data) err)))
+
+        ;(setf data_index (remove-nth rand_index data_index))
+        )
+
+  ;(setf output
+  ;weights
+  ;current_data
+  ;)
+     )
+      )
+
+  ;output
+  ;weights
+   ))
