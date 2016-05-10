98de43bfd24e77d2517d2b4f2ba29cfa1d64c956
Martin Keršner <m.kersner@gmail.com>
2016-05-10 16:56:27 +0900

    finished knn; lacks verification

--- knn.lisp
+++ knn.lisp
@@ -3,8 +3,47 @@
 ;;;;
 ;;;; k-Nearest Neighbors 
 
-(load "math2")
+;;; TODO
+;;; prepare dataset specially for knn
+;;; and also include tests for algorithm verification
 
-(defun knn (data_row, data_mat, labels_mat k)
-  (power 2 (subtract-row data_mat data_row))
-  )
+(load "math2")
+(load "dataset2")
+
+; training data preparation
+(defparameter *label-col* 2)
+
+(defparameter *data* (remove-col *label-col* *dataset2*))
+(defparameter *labels* (matrix-from-data (nth-col *label-col* *dataset2*)))
+
+(defparameter *row-data* (matrix-from-data '((2 5))))
+;(defparameter *row-data* (matrix-from-data '((0 8))))
+
+(defun knn (data_row data_mat labels_mat k)
+  (let* ((class_count (make-hash-table))
+         (labels (matrix-data labels_mat))
+         (k_neighbors (subseq (arg-sort-col-mat ; take only the closest k data points
+                        (power 0.5 (sum-rows (power 2 (subtract-row data_mat data_row))))) ; Euclidean distance
+                       0 k))
+         (k_labels (mapcar #'(lambda (x) (car (nth x labels))) k_neighbors))
+         (final_class 0)
+         (max_count 0))
+
+    ;; count number of class occurences
+    (mapcar #'(lambda (x)
+                (let ((val (gethash x class_count)))
+                   (if val
+                     (setf (gethash x class_count) (+ 1 val))
+                     (setf (gethash x class_count) 1))
+                   ))
+            k_labels)
+
+    ;; find class with the highest occurence
+    (maphash #'(lambda (k v)
+                  (if (> v max_count)
+                      (progn
+                        (setf max_count v)
+                        (setf final_class k))))
+             class_count)
+
+    final_class))
