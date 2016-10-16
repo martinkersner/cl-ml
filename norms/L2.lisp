592c968b1afec87983cbd9f449e0a092d1d34c34
Martin <m.kersner@gmail.com>
2016-10-16 15:44:23 +0900

    L2 norm

---
+++ norms/L2.lisp
@@ -0,0 +1,43 @@
+;;;; Martin Kersner, m.kersner@gmail.com
+;;;; 2016/10/16
+;;;;
+;;;; L2 norm of row-oriented data
+;;;
+;;; TODO recognize orientation of matrices (row vs col subtraction)
+
+(in-package :lispml)
+
+; Compute L2 distance between X1 and X2.
+; Number of rows of X1 should be the same or larger than X2.
+(defun L2 (X1 X2 &key (precise NIL))
+  (if (= (matrix-rows X2) (matrix-rows X1))
+    (L2-single-row    X1 X2 :precise precise)
+    (L2-multiple-rows X1 X2 :precise precise)))
+
+; Single rows
+(defun L2-single-row (X1 X2 &key precise)
+  (if precise
+    (L2-single-row-precise X1 X2)
+    (L2-single-row-approx  X1 X2)))
+
+; TODO unit tests
+(defun L2-single-row-precise (X1 X2)
+  (expt (sum (power 2 (subtract X1 X2))) 0.5))
+
+; TODO unit tests
+(defun L2-single-row-approx (X1 X2)
+  (sum (power 2 (subtract X1 X2))))
+
+; Multiple rows
+(defun L2-multiple-rows (X1 X2 &key precise)
+  (if precise
+    (L2-multiple-rows-precise X1 X2)
+    (L2-multiple-rows-approx  X1 X2)))
+
+; TODO unit tests
+(defun L2-multiple-rows-precise (X1 X2)
+  (power 0.5 (sum-rows (power 2 (subtract-row X1 X2)))))
+
+; TODO unit tests
+(defun L2-multiple-rows-approx (X1 X2)
+  (sum-rows (power 2 (subtract-row X1 X2))))
