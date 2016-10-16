01d482df9e8fa33ad933179d52816bd93aa125e9
Martin <m.kersner@gmail.com>
2016-10-16 13:46:39 +0900

    check if hash table was defined

--- utils.lisp
+++ utils.lisp
@@ -100,8 +100,10 @@
 ;;; Read item from hash table of parameters. If sought key does not exist return
 ;;; default value for particular parameter.
 (defun read-param (ht-params param-name default-val)
-  (progn
-    (multiple-value-setq (val found) (gethash param-name ht-params))
-    (if (not found)
-      default-val
-      val)))
+  (if (not ht-params)
+    default-val
+    (progn
+      (multiple-value-setq (val found) (gethash param-name ht-params))
+      (if (not found)
+        default-val
+        val))))
