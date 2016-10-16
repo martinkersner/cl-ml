8d532e1befddbdc4e56b51678df3ea4b8105823a
Martin <m.kersner@gmail.com>
2016-10-16 12:43:40 +0900

    cleaning deprecated functions

--- linear-regression/linear-regression.lisp
+++ linear-regression/linear-regression.lisp
@@ -2,31 +2,10 @@
 ;;;; 2016/08/12
 ;;;;
 ;;;; Linear regression.
+;;;
+;;; TODO accept parameter for selecting optimization method
 
 (in-package :lispml)
-
-;;; 1-D linear regression
-;;; Deprecated
-;(defun train-linear-regression-1D (data-lst labels-lst)
-;  (let* ((mean-x  (mean data-lst))
-;         (mean-y  (mean labels-lst))
-;         (var-x   (var data-lst :ddof 1))
-;         (cov-x-y (cov data-lst labels-lst))
-;
-;         (beta    (/ cov-x-y var-x))
-;         (alpha   (- mean-y (* beta mean-x))))
-;
-;    (list alpha beta)))
-;
-;(defun predict-linear-regression-1D (alpha beta x)
-;  (+ alpha (* beta x)))
-;
-;(defun test-linear-regression-1D (alpha beta data-lst labels-lst)
-;  (let* ((ss-res (apply #'+ (mapcar #'(lambda (x y) (expt (- y (predict-linear-regression-1D alpha beta x)) 2)) data-lst labels-lst)))
-;         (mean-y (mean labels-lst))
-;         (ss-tot (apply #'+ (mapcar #'(lambda (y) (expt (- y mean-y) 2)) labels-lst))))
-;
-;    (- 1 (/ ss-res ss-tot))))
 
 ;;; n-D linear regression
 (defclass linear-regression ()
@@ -35,7 +14,6 @@
 (defgeneric fit (linreg X y &optional params)
   (:documentation "Fit linear model."))
 
-;;; TODO take num-epoch and lr out of method
 (defmethod fit ((linreg linear-regression) X y &optional params)
   (let ((X (prefix-const-val 1.0 X))
         (num-epoch (gethash 'num-epoch params))
