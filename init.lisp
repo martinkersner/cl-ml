f53d29fb093eb6a3bf83c56225f9cf22ce54563f
Martin Kersner <m.kersner@gmail.com>
2017-02-05 15:20:45 +0900

    remove asdf submodule

--- init.lisp
+++ init.lisp
@@ -1,8 +1,6 @@
 ;;;; Martin Kersner, m.kersner@gmail.com
 ;;;; 2016/09/16
 
-(load "asdf/build/asdf")
-
 (push (namestring (ext:cd)) asdf:*central-registry*)
 (push "plot/" asdf:*central-registry*)
 (push "math/" asdf:*central-registry*)
